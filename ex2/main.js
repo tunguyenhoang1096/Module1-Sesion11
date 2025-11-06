1ddaef1de724a0e5461dd85c938ee66b9b798a4c
tunguyenhoang1096 <tunguyenhoang1096@gmail.com>
2025-11-06 23:56:59 +0900

    Add files via upload

---
+++ ex2/main.js
@@ -0,0 +1,7 @@
+let text = prompt("What is the “official” name of JavaScript?");
+
+while (text != "ECMAScript") {
+  alert("Didn’t know? ECMAScript!");
+  text = prompt("What is the “official” name of JavaScript?");
+}
+alert("Right!");
